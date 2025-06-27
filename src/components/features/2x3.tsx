613fa49bdfffb1fe0e069f26b057e0849e6542e4
mattisk <mattiskubisa@gmail.com>
2025-06-27 16:26:31 +0200

    removed unused useState from 3x2.tsx

--- src/components/features/2x3.tsx
+++ src/components/features/2x3.tsx
@@ -3,7 +3,6 @@
 import { Shield, Palette, Globe, Users, BookOpen, Zap } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { useState } from "react"
 
 export default function Projekterfolg() {
 
